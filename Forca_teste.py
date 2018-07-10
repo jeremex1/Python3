34e6d4751a189549f140cf32337ea79a99ea0496
jeremias <jeremiaspereira744@gmail.com>
2018-07-10 19:25:37 -0300

    primeiros projetos

---
+++ Forca_teste.py
@@ -0,0 +1,108 @@
+
+class Forca:
+    def __init__(self):
+        self.palavra = input('enter com um palavra\n').upper().strip()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        
+        self.digitadas = []
+        self.certas = []
+        self.erro = 0
+       
+    def draw_erro(self):
+        if self.erro == 1:
+            
+            print('###( )###')
+        elif self.erro == 2:
+            print('###( )###')
+            print('    |    ')
+        elif self.erro == 3:
+            print('###( )###')
+            print('    |    ')
+            print('  \ | /  ')
+        elif self.erro == 4:
+            print('###( )###')
+            print('    |    ')
+            print('  \ | /  ')
+            print('   \ /   ')
+        elif self.erro == 5:
+            print('###( )###')
+            print('    |    ')
+            print('  \ | /  ')
+            print('   \ /   ')
+            print('   / \   ')
+            print('  /   \  ')
+            
+            return print('Enforcado')
+    
+
+    def jogar(self):
+
+        while True:
+        
+            if self.erro == 5:
+                print(self.digitadas)
+                break
+                return False
+            
+            self.key = ""
+            
+            for letra in self.palavra:
+                self.key+= letra if letra in self.certas else '.'
+                
+            print(self.key,'\n')
+            
+            if self.key == self.palavra:
+                return print('Ganhou')
+                
+            self.chute = input('digite uma letra\n').upper().strip()
+            
+            if self.chute in self.digitadas:
+                print('Voce ja digitou essa\n')
+                continue   
+            else:
+                self.digitadas+=self.chute
+                
+                if self.chute in self.palavra:
+                    self.certas += self.chute
+                    
+                else:
+                    self.erro +=1
+                    self.draw_erro()
+
+x = Forca()
+x.jogar()
