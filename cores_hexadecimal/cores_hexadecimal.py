5d1687f352d9a1a55a4405986aca3f0dcb115a35
jeremex1 <jeremiaspereira744@gmail.com>
2020-01-02 11:13:11 -0300

    melhora na inplementacao das funcoes

--- cores_hexadecimal/cores_hexadecimal.py
+++ cores_hexadecimal/cores_hexadecimal.py
@@ -1,6 +1,6 @@
 
+from tkinter import Frame,Label
 import tkinter as tk
-from tkinter import *
 import material_design_colors as c
 
 class Cores(tk.Frame,c.Hexa):
@@ -9,45 +9,42 @@ class Cores(tk.Frame,c.Hexa):
 		self.master.geometry("850x530+200+200")
 		#self.master.resizable(width=False, height=False)
 		self.master.title("Cores_Hexadecimal")
-		self.Frame()
+		self.frame()
 		self.master.mainloop()
 
-	def Frame(self):
+	def frame(self):
 		self.frame1=tk.Frame(self.master,width=845,height=530,bd=5,relief='raise')
-		self.frame1.pack(side=TOP)
+		self.frame1.pack(side="top")
 		self.cores1()
-		self.Label(self.red,0,0)
-		self.Label(self.pink,0,1)
-		self.Label(self.purple,0,2)
-		self.Label(self.deep_purple,0,3)
-		self.Label(self.indigo,0,4)
-		self.Label(self.blue,0,5)
-		self.Label(self.light_blue,0,6)
-		self.Label(self.cyan,0,7)
-		self.Label(self.teal,0,8)
-		self.Label(self.black,0,9)
-		self.Label(self.green,18,0)
-		self.Label(self.light_green,18,1)
-		self.Label(self.lime,18,2)
-		self.Label(self.yellow,18,3)
-		self.Label(self.amber,18,4)
-		self.Label(self.orange,18,5)
-		self.Label(self.deep_orange,18,6)
-		self.Label(self.brown,18,7)
-		self.Label(self.grey,18,8)
-		self.Label(self.blue_grey,18,9)
+		self.label(self.red,0,0)
+		self.label(self.pink,0,1)
+		self.label(self.purple,0,2)
+		self.label(self.deep_purple,0,3)
+		self.label(self.indigo,0,4)
+		self.label(self.blue,0,5)
+		self.label(self.light_blue,0,6)
+		self.label(self.cyan,0,7)
+		self.label(self.teal,0,8)
+		self.label(self.black,0,9)
+		self.label(self.green,18,0)
+		self.label(self.light_green,18,1)
+		self.label(self.lime,18,2)
+		self.label(self.yellow,18,3)
+		self.label(self.amber,18,4)
+		self.label(self.orange,18,5)
+		self.label(self.deep_orange,18,6)
+		self.label(self.brown,18,7)
+		self.label(self.grey,18,8)
+		self.label(self.blue_grey,18,9)
 		
-	def Label(self,tons,linha,coluna):
-		
+	def label(self,tons,linha,coluna):
 		self.tons =tons
-		cor = []
 		linha =linha
 		coluna = coluna
-		for c in self.tons.values():
-			cor.append(c)
+		cor =[cor for cor in self.tons.values() ]
 		for lab in cor:
-			self.label = tk.Label(self.frame1,width=10,text=lab,bg=lab)
-			self.label.grid(row=linha,column=coluna)
+			self.label2 = tk.Label(self.frame1,width=10,text=lab,bg=lab)
+			self.label2.grid(row=linha,column=coluna)
 			linha +=1
-
-x = Cores()
+if __name__=="__main__":
+	x = Cores()
